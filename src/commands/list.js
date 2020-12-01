1daba4d1703356e560a92444c2465ed2ccf3ad29
Victor <victorlobejr@hotmail.com>
2020-12-01 12:54:21 -0800

    added some spacing for readability

--- src/commands/list.js
+++ src/commands/list.js
@@ -5,15 +5,15 @@ module.exports = {
     name: 'list',
     description: 'List players online',
     async execute(message) {
-
+        let embed = new Discord.MessageEmbed()
+            .setTitle("Player List")
         try {
             await fetch('http://play.totalfreedom.me:28966/list?json=true');
         } catch {
-            const embed = new Discord.MessageEmbed()
-                .setDescription("Server is offline.");
-                
-                return message.channel.send(embed);
+                embed.setDescription("Server is offline.");
+            return message.channel.send(embed);
         }
+
         let players = await fetch('http://play.totalfreedom.me:28966/list?json=true');
         players = await players.json();
 
@@ -29,26 +29,27 @@ module.exports = {
         };
 
         if (players.online === 0) {
-            let embed = new Discord.MessageEmbed()
-                .setTitle("Player List")
-                .setDescription("There are no players online.");
 
+            embed.setDescription("There are no players online.");
             return message.channel.send(embed);
+
         }
 
-        let embed = new Discord.MessageEmbed()
-            .setTitle("Player List")
-            .setDescription(`There are ${players.online} / ${players.max} players online.`);
+        embed.setDescription(`There are ${players.online} / ${players.max} players online.`);
 
         for (let x in onlinePlayers) {
+
             if (onlinePlayers[x].length !== 0) {
                 let rank = x.charAt(0).toUpperCase() + x.slice(1)
                 embed.addFields({
                     name: `${rank.match(/[A-Z][a-z]+|[0-9]+/g).join(' ')} (${onlinePlayers[x].length})`,
                     value: onlinePlayers[x].join(', '),
                 });
+
             }
+
         }
+
         return message.channel.send(embed);
     }
 }
